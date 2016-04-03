5dfe1c8138f2b96aa316d76934a5f287ee8b155c
HarriKnox <harri.alpha96@gmail.com>
2016-04-03 02:04:00 -0700

    Decided to put everything into a table for organization purposes

--- TicketToRideScoreCalculator.js
+++ TicketToRideScoreCalculator.js
@@ -2,19 +2,28 @@
 {
 	$(document).ready(function()
 	{
-		var $scColorButtonRow = $('<tr class="ticket-to-ride-score-calculator-color-button-row"></tr>');
-		var $scColorInputRow = $('<tr class="ticket-to-ride-score-calculator-color-input-row"></tr>');
-		var $scColorScoreRow = $('<tr class="ticket-to-ride-score-calculator-color-score-row"></tr>');
+		var TTRSC = "ticket-to-ride-score-calculator";
 		
-		var $scDiv = $('.ticket-to-ride-score-calculator-div').append('<table><tbody class="ticket-to-ride-score-calculator-table-body"></tbody></table>');
-		$('.ticket-to-ride-score-calculator-table-body').append($scColorButtonRow).append($scColorInputRow).append($scColorScoreRow);
+		var $scTableHeaderRow = $('<tr class="' + TTRSC + '-table-header"><th></th></tr>');
+		var $scTableHead = $('<thead></thead>').append($scTableHeaderRow);
+		var $scTableBody = $('<tbody class="' + TTRSC + '-table-body"></tbody>');
+		var $scTable = $('<table></table>').append($scTableHead).append($scTableBody);
+		var $scDiv = $('.' + TTRSC + '-div').append($scTable);
 		
 		
 		var colors = ['Black', 'Blue', 'Green', 'Red', 'Yellow'];
+		var fields = ['One-Train Tracks', 'Two-Train Tracks', 'Three-Train Tracks', 'Four-Train Tracks', 'Five-Train Tracks', 'Six-Train Tracks', 'Longest Track Length', 'Destinations Reached Points', 'Destinations Failed Points', 'Extra Points'];
+		var fieldNamesFormatted = fields.map(function(field) { return field.toLowerCase().replace(/\s+/, "-"); })
+		var fieldRowClasses = fieldNamesFormatted.map(function(fieldName) { return TTRSC + '-' + fieldName + '-row'; });
+		
+		fields.forEach(function(field)
+		{
+			//$scTableBody.append('<tr class="
+		});
 		
 		colors.forEach(function(color)
 		{
-			$scColorButtonRow.append('<td><input type="checkbox" name="color" value="' + color + '" /> ' + color + '</td>');
+			$scTableHeaderRow.append('<th>' + color + '</th>');
 			
 		});
 	});
