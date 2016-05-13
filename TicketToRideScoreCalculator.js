261cd24c42d54525e2aae96248a6224bd66f59d5
HarriKnox <harri.alpha96@gmail.com>
2016-05-12 17:50:02 -0700

    Added functions for score calculation and elements for score display

--- TicketToRideScoreCalculator.js
+++ TicketToRideScoreCalculator.js
@@ -1,18 +1,20 @@
 (function()
 {
-	if (typeof scLoaded === 'undefined')
+	if (typeof ticketToRideCalculateScores === 'undefined')
 	{
 		var TTRSC = 'ticket-to-ride-score-calculator-';
-		var nameFormatter = function(field) { return TTRSC + field.toLowerCase().replace(/\s+/, "-") + '-'; };
+		var nameFormatter = function(field) { return TTRSC + field.toLowerCase().replace(/\s+/g, '-') + '-'; };
 		
 		var colors = ['Black', 'Blue', 'Green', 'Red', 'Yellow'];
-		var colorNames = colors.map(function(color) { return color.toLowerCase(); });
+		 colorNames = colors.map(function(color) { return color.toLowerCase(); });
 		
 		var quantityFields = ['One-Train Tracks', 'Two-Train Tracks', 'Three-Train Tracks', 'Four-Train Tracks', 'Five-Train Tracks', 'Six-Train Tracks', 'Longest Track Length'];
 		var quantityFieldNames = quantityFields.map(nameFormatter);
 		
-		var textFields = ['Destinations Reached Points', 'Destinations Failed Points', 'Extra Points'];
+		var textFields = ['Destinations Reached Points', 'Destinations Failed Points', 'Extra Points Won or Lost'];
 		var textFieldNames = textFields.map(nameFormatter);
+		
+		var allFieldNames = quantityFieldNames.concat(textFieldNames);
 		
 		var headerRow = document.createElement('tr');
 		headerRow.className = TTRSC + 'table-header';
@@ -47,6 +49,7 @@
 				fieldInput.value = 0;
 				fieldInput.min = 0;
 				fieldInput.className = field + color;
+				fieldInput.style = 'width:50px;';
 				
 				var fieldCell = document.createElement('td');
 				fieldCell.appendChild(fieldInput);
@@ -67,9 +70,10 @@
 			{
 				var fieldInput = document.createElement('input');
 				fieldInput.type = 'text';
-				fieldInput.pattern = '^\\d+(\\s+\\d+)*$';
+				fieldInput.pattern = '^-?\\d+(\\s+-?\\d+)*$';
 				fieldInput.value = 0;
 				fieldInput.className = field + color;
+				fieldInput.style = 'width:50px;';
 				
 				var fieldCell = document.createElement('td');
 				fieldCell.appendChild(fieldInput);
@@ -78,9 +82,97 @@
 			tableBody.appendChild(fieldRow);
 		});
 		
+		var colorIsValid = function(color)
+		{
+			for (var i = 0, len = allFieldNames.length; i < len; i++)
+			{
+				var field = allFieldNames[i];
+				if (!(document.getElementsByClassName(field + color)[0].validity.valid))
+					return false;
+			}
+			return true;
+		}
+		
+		var longestTrackColors = function()
+		{
+			var maxLength = 0;
+			var longestColors = [];
+			var longestRouteClassNamePrefix = quantityFieldNames[6];
+			
+			colorNames.forEach(function(color)
+			{
+				var length = document.getElementsByClassName(longestRouteClassNamePrefix + color)[0].valueAsNumber;
+				if (!isNaN(length))
+				{
+					if (length > maxLength)
+					{
+						maxLength = length;
+						longestColors = [color];
+					}
+					else if (length == maxLength)
+					{
+						longestColors.push(color);
+					}
+				}
+			});
+			return longestColors;
+		}
+		
+		ticketToRideCalculateScores = function()
+		{
+			var longestTracks = longestTrackColors();
+			var scorePrefix = TTRSC + 'scores-';
+			
+			colorNames.forEach(function(color)
+			{
+				var scoreCell = document.getElementsByClassName(scorePrefix + color)[0];
+				if (colorIsValid(color))
+				{
+					var score = 0;
+				}
+				else
+				{
+					scoreCell.innerHTML = '<span style="color:#f00;">error</span>';
+				}
+			});
+		}
+		
+		var tableFoot = document.createElement('tfoot');
+		
+		var calculateButton = document.createElement('input');
+		calculateButton.type = 'button';
+		calculateButton.style = 'width:100%;';
+		calculateButton.value = 'Calculate Scores';
+		
+		var calculateButtonTD = document.createElement('td');
+		calculateButtonTD.appendChild(calculateButton);
+		calculateButtonTD.colSpan = 5;
+		
+		var calculateButtonRow = document.createElement('tr');
+		calculateButtonRow.appendChild(document.createElement('td'));
+		calculateButtonRow.appendChild(calculateButtonTD);
+		tableFoot.appendChild(calculateButtonRow);
+		
+		var scoresRow = document.createElement('tr');
+		var scoresLabel = document.createElement('td');
+		scoresLabel.innerText = 'Scores';
+		scoresLabel.style = 'font-style:italic;text-align:right;';
+		scoresRow.appendChild(scoresLabel);
+		
+		colorNames.forEach(function(color)
+		{
+			var scoresRowResult = document.createElement('th');
+			scoresRowResult.className = TTRSC + 'scores-' + color;
+			scoresRowResult.style = 'font-weight:bold;';
+			scoresRowResult.innerText = '0';
+			scoresRow.appendChild(scoresRowResult);
+		});
+		tableFoot.appendChild(scoresRow);
+		
 		var table = document.createElement('table');
 		table.appendChild(tableHead);
 		table.appendChild(tableBody);
+		table.appendChild(tableFoot);
 		
 		var parent = (function(coll) { return coll[coll.length - 1]; })(document.getElementsByTagName('script')).parentNode;
 		parent.appendChild(table);
